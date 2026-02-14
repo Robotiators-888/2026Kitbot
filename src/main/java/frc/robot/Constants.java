3131e421915e01e9732223fd7d2f25e158215065
Aikam <sidhparikh@gmail.com>
2026-02-14 11:25:56 -0500

    Changed voltage and spin up

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -41,10 +41,10 @@ public final class Constants {
     // See the Software Guide for tuning information
     public static final double INTAKING_FEEDER_VOLTAGE = -12;
     public static final double INTAKING_INTAKE_VOLTAGE = 10;
-    public static final double LAUNCHING_FEEDER_VOLTAGE = 10;
-    public static final double LAUNCHING_LAUNCHER_VOLTAGE = 12;
+    public static final double LAUNCHING_FEEDER_VOLTAGE = 12;
+    public static final double LAUNCHING_LAUNCHER_VOLTAGE = 16;
     public static final double SPIN_UP_FEEDER_VOLTAGE = 6;
-    public static final double SPIN_UP_SECONDS = 1;
+    public static final double SPIN_UP_SECONDS = .25;
   }
 
   public static final class OperatorConstants {
