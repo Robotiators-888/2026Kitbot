de3485ce29e5eb0fdeb58e79fdb3185d9cec2ec8
OmarKarim747 <superoctopus747@gmail.com>
2026-03-02 09:16:44 -0500

    Changed Motor to a brushless

--- src/main/java/frc/robot/subsystems/IntakeLauncherSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeLauncherSubsystem.java
@@ -1,17 +1,21 @@
 package frc.robot.subsystems;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.FuelConstants.*;
 public class IntakeLauncherSubsystem extends SubsystemBase {
-    private Spark intakeLauncherRoller;
+    private SparkMax intakeLauncherRoller;
     public Object stop;
     
         public IntakeLauncherSubsystem() {
     
-            intakeLauncherRoller = new Spark(INTAKE_LAUNCHER_MOTOR_ID);
+            intakeLauncherRoller = new SparkMax(INTAKE_LAUNCHER_MOTOR_ID, MotorType.kBrushless);
     
         }
     
