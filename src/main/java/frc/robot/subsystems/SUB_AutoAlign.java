06f65f3969637cd815a107acc0a2f64be2d7b3a3
OmarKarim747 <superoctopus747@gmail.com>
2026-03-30 15:38:36 -0400

    test

--- src/main/java/frc/robot/subsystems/SUB_AutoAlign.java
+++ src/main/java/frc/robot/subsystems/SUB_AutoAlign.java
@@ -2,6 +2,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SUB_AutoAlign {
     private final SUB_Drivetrain driveSubsystem = SUB_Drivetrain.getInstance();
@@ -10,4 +13,12 @@ public class SUB_AutoAlign {
     private SUB_AutoAlign() {
         Bluehub = new Pose2d(Units.inchesToMeters(181.56), Units.inchesToMeters(158.32), null);
     }
+
+        StructPublisher<Pose2d> hubposepuPublisher = NetworkTableInstance.getDefault()
+    .getStructTopic("HubPose",Pose2d.struct).publish();
+
+    public void periodic() {
+        hubposepuPublisher.set(Bluehub);
+    }
+
 }
